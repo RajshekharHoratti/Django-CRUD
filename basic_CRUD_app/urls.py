1c81ae082d61bdd5b0e74e1e430d435e1287546d
rajhoratti <rajhoratti@mobifyi.com>
2019-08-05 13:31:42 +0530

    user module conencted and adding task list from user and superadmin working

--- basic_CRUD_app/urls.py
+++ basic_CRUD_app/urls.py
@@ -4,16 +4,21 @@ from basic_CRUD_app import views
 app_name = 'basic_CRUD_app'
 
 urlpatterns = [
+    # -------------------------------------------Templates-------------------------------------------------------------
     url(r'^$', views.login_page, name='signin'),
-    url(r'^superadmin/dashboard/(?P<id>[\w\-]+)$', views.admin_dashboard_page, name='admin_dashboard_page'),
 
     url(r'^superadmin/tasks/(?P<id>[\w\-]+)$', views.admin_tasks_page, name='admin_tasks_page'),
+    url(r'^addTask/(?P<id>[\w\-]+)$', views.admin_add_tasks_page, name='admin_add_tasks_page'),
+    url(r'^addTaskLogic/(?P<id>[\w\-]+)$', views.addTaskLogic, name='addTaskLogic'),
 
 
 
 
+    url(r'^user/tasks/(?P<id>[\w\-]+)$', views.user_tasks_page, name='user_tasks_page'),
 
-
+    # -------------------------------------------API-------------------------------------------------------------
     url(r'^login/$', views.LoginAPI.as_view(), name='LoginAPI'),
+    url(r'^tasks/', views.TasksAPI.as_view(), name='TasksAPI'),
+    url(r'^taskAction/(?P<id>[\w\-]+)$', views.TaskActionAPI.as_view(), name='TaskActionAPI'),
 
 ]
--- basic_CRUD_app/views.py
+++ basic_CRUD_app/views.py
@@ -2,6 +2,11 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from basic_CRUD_main.settings import db
+from bson import ObjectId
+import time
+from datetime import datetime
+
+
 
 
 
@@ -11,13 +16,37 @@ def login_page(request):
     return render(request, 'dashboard/login.html')
 
 
-def admin_dashboard_page(request, id):
-    return render(request, 'dashboard/adminDashboard.html', context={"id": id})
-
-
-
 def admin_tasks_page(request, id):
-    return render(request, 'dashboard/adminDashboard.html', context={"id": id})
+    return render(request, 'dashboard/superadmin_tasks.html', context={"id": id})
+
+def user_tasks_page(request, id):
+    return render(request, 'dashboard/user_tasks.html', context={"id": id})
+
+
+
+def admin_add_tasks_page(request, id):
+    return render(request, 'dashboard/addTasks.html', context={"id": id})
+
+
+def addTaskLogic(request, id):
+    data = {
+        "userId": ObjectId(id),
+        "title": request.POST.get('title'),
+        "description": request.POST.get('desc'),
+        "timing": request.POST.get('startdate'),
+        "statusCode": 0,
+        "statusMsg": "pending",
+        "createdAt": time.time(),
+        "modifiedAt": "",
+        "isDeleted": False
+    }
+    db.tasks.insert(data)
+    detailsDB = db.superAdmin.find({"_id": ObjectId(id)}).count()
+    if detailsDB > 0:
+        return redirect('/superadmin/tasks/'+id)
+    else:
+        return redirect('/user/tasks/'+id)
+
 
 
 class LoginAPI(APIView):
@@ -40,6 +69,7 @@ class LoginAPI(APIView):
                         'id': str(item['_id']),
                         'identity': int(request.data['identity'])
                     }
+                    print(data)
                     return JsonResponse(data, safe=False, status=200)
             else:
                 data = {
@@ -52,3 +82,98 @@ class LoginAPI(APIView):
                 "message": "Internal Server Error"
             }
             return JsonResponse(data, safe=False, status=500)
+
+class TasksAPI(APIView):
+    def get(self, request):
+        '''
+                API to list out all the tasks
+
+        '''
+        # try:
+        if 'HTTP_USERID' in request.META:
+            tasksDB = db.tasks.find({"userId": ObjectId(request.META['HTTP_USERID']), "isDeleted": False})
+        else:
+            tasksDB = db.tasks.find({})
+        if tasksDB.count() > 0:
+            data = []
+            for item in tasksDB:
+                usersDB = db.users.find({"_id": item['userId']})
+                if usersDB.count() > 0:
+                    for user in usersDB:
+                        data.append({
+                            "id": str(item['_id']),
+                            "userId": str(item['userId']),
+                            "userName": str(user['email']),
+                            "title": str(item['title']),
+                            "description": str(item['description']),
+                            "timing": str(item['timing']),
+                            "statusMsg": str(item['statusMsg']),
+                            "createdAt": datetime.utcfromtimestamp(item['createdAt']).strftime('%Y-%m-%d %H:%M:%S'),
+                            "modifiedAt": datetime.utcfromtimestamp(item['modifiedAt']).strftime(
+                                '%Y-%m-%d %H:%M:%S') if len(str(item['modifiedAt'])) > 0 else "",
+                            "isDeleted": str(item['isDeleted']),
+                        })
+                else:
+                    print("no user found..!!")
+                    usersDB = db.superAdmin.find({"_id": item['userId']})
+                    if usersDB.count() > 0:
+                        for user in usersDB:
+                            data.append({
+                                "id": str(item['_id']),
+                                "userId": str(item['userId']),
+                                "userName": str(user['email']),
+                                "title": str(item['title']),
+                                "description": str(item['description']),
+                                "timing": str(item['timing']),
+                                "statusMsg": str(item['statusMsg']),
+                                "createdAt": datetime.utcfromtimestamp(item['createdAt']).strftime('%Y-%m-%d %H:%M:%S'),
+                                "modifiedAt": datetime.utcfromtimestamp(item['modifiedAt']).strftime(
+                                    '%Y-%m-%d %H:%M:%S') if len(str(item['modifiedAt'])) > 0 else "",
+                                "isDeleted": str(item['isDeleted']),
+                            })
+                    else:
+                        print("no user or superadmin found..!!")
+
+            response_message = {
+                "message": "Authorized User..!!",
+                'data': data,
+            }
+            return JsonResponse(response_message, safe=False, status=200)
+        else:
+            data = {
+                "message": "No Data Found"
+            }
+
+            return JsonResponse(data, safe=False, status=404)
+        # except:
+        #     data = {
+        #         "message": "Internal Server Error"
+        #     }
+        #     return JsonResponse(data, safe=False, status=500)
+
+
+class TaskActionAPI(APIView):
+    def delete(self, request, id):
+        '''
+                API to delete tasks
+
+        '''
+        detailsDB = db.tasks.find({'_id': ObjectId(id)})
+        if detailsDB.count() > 0:
+            db.tasks.update_one({
+                '_id': ObjectId(id)
+            }, {
+                '$set': {
+                    'isDeleted': True,
+                }
+            }, upsert=False)
+
+            data = {
+                "message": "task deleted..!!",
+            }
+            return JsonResponse(data, safe=False, status=200)
+        else:
+            data = {
+                "message": "No data found"
+            }
+            return JsonResponse(data, safe=False, status=404)
